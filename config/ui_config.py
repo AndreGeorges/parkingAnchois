8749d44a1f6bd5ac334ae5d84854a118e8240e43
lpleboeuf <lpleboeuf@gmail.com>
2026-06-07 18:47:06 -0400

    certains commentaires qui ne semblaient pas avoir suivi et fichier requirement opur aller avec le readme

--- config/ui_config.py
+++ config/ui_config.py
@@ -7,15 +7,15 @@ def get_tarif(duree, TARIFS):
 def get_screen_color(etat_pg, SCREEN_COLOR):
     return SCREEN_COLOR.get(etat_pg)
 
-# cette fonction permet de retourner la couleur
+#  cette fonction permet de retourner la couleur
 def get_color(color_name):
     return COLOR[color_name]
 
-# pour transformer le rgb en hexadecimal pour que tkinter comprenne la couleur
+#  pour transformer le rgb en hexadecimal pour que tkinter comprenne la couleur
 def get_color_rgb(color_name):
     rgb = COLOR[color_name]
     return f"#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}"
 
-# J'aurais surement pu juste passer par config_loader.. but here we are......
+#  J'aurais surement pu juste passer par config_loader.. but here we are......
 def get_AUTH():
     return AUTH_VIEW
